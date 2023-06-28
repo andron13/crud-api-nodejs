19009c6f6bc8068b1613d3d47c6a2544d3876e2a
andron13 <andron13@gmail.com>
2023-06-28 10:16:25 +0200

    feat: implement production and development modes correct: start:prod, start:dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const {NODE_ENV = 'production'} = process.env;
 
 module.exports = {
-  mode: 'production',
+  mode: NODE_ENV,
   entry: './src/app.js',
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', // Имя файла сборки
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
 };
